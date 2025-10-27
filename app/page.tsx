b0d8d1db8ad9a8a1fd94db9f7dab7b718a226af4
HadiAbu <hadi.abuhamed@gmail.com>
2025-10-27 16:33:39 +0900

    updated public folder and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Hello from "./components/Hello";
 
 const Home = () => {
-  return <div className="text-5xl underline">Welcome to Paradise</div>;
+  return (
+    <>
+      <div className="text-5xl underline">{`Welcome to Paradise`}</div>
+      <br />
+
+      <Hello />
+    </>
+  );
 };
 
 export default Home;
