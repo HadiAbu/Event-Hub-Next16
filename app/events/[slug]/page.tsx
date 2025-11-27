b7ed27f2334fdc047097742b2fa582c011b2fa31
HadiAbu <hadi.abuhamed@gmail.com>
2025-11-27 20:50:25 +0900

    slug must be awaited

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+type RouteParams = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
 
-const EventDetailsPage = () => {
-  return <div>page</div>;
+// another way to type params
+// const EventDetailsPage = async ({params}:{params: Promise<{slug: string}>}) => {
+const EventDetailsPage = async ({ params }: RouteParams) => {
+  const { slug } = await params; // slug must be awaited
+  return <div>page - {`${slug}`}</div>;
 };
 
 export default EventDetailsPage;
